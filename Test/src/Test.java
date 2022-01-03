fbde4beddb4c659ca028223bf2d4d738b4f24a1a
pranjalikharat111 <pranjalikharat111@gmail.com>
2022-01-03 15:52:10 +0530

    String Program

---
+++ Test/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "Yes";
+		if(hungry=="yes") {
+			System.out.println("plz eat something");
+		}else {
+			System.out.println("do something  ");
+		}
+
+			}
+
+	}
+
+
