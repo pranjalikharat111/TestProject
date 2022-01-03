32d5a69e4e00ac2b1f919e4fb5ef7ac97f0b8923
pranjalikharat111 <pranjalikharat111@gmail.com>
2022-01-03 21:39:04 +0530

    change

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,17 +1,20 @@
 
+
+
 public class Test {
 
-	public static void main(java.lang.String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String hungry = "Yes";
-		if(hungry=="yes") {
-			System.out.println("plz eat something");
-		}else {
-			System.out.println("do something  ");
-		}
-
-			}
-
+int list[]= {22,15,37,89,78,90};
+int max1=0,max2=0;
+for(int num:list) {
+	if(max1<num) {
+		max2=max1;
+		max1=num;
+	}else if(max2<num) {
+		max2=num;
+		
 	}
-
-
+}System.out.println("max1-"+max1+"\tmax2-"+max2);
+}
+}
